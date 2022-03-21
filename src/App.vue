dbb07c6b4dcd9bb8e9d3483c65eabc4331a4d950
KJ Jenkins <99845714+KJinkies@users.noreply.github.com>
2022-03-22 09:05:34 +1300

    Added files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import SeanComponent from './components/sean-component.vue';
+import KjComponent from './components/kj-component.vue';
+import MustafaComponent from './components/mustafa-component.vue';
+import MaxComponent from './components/max-component.vue';
+import SunnyComponent from './components/sunny-component.vue';
+</script>
+
+<template>
+ 
+<SeanComponent/>
+
+<KjComponent/>
+
+<MustafaComponent/>
+
+<MaxComponent/>
+
+<SunnyComponent/>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/sean-component.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>this is your component pls edit</h1>
+</template>
+
+<style>
+
+</style>
